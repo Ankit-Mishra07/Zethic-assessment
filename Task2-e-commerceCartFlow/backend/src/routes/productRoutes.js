1ea20b7c0ad8f1b90a4633e6e5be10da73562162
Ankit Mishra <90400569+Ankit-Mishra07@users.noreply.github.com>
2022-09-09 00:55:21 +0530

    Authentication middleware for creating product

--- Task2-e-commerceCartFlow/backend/src/routes/productRoutes.js
+++ Task2-e-commerceCartFlow/backend/src/routes/productRoutes.js
@@ -3,10 +3,11 @@ import {
   createProduct,
   getAllProducts,
 } from "../controllers/products.controller.js";
+import protect from "../middlewares/authMiddleware.js";
 
 const productRouter = Router();
 
 productRouter.route("/allproducts").get(getAllProducts);
-productRouter.route("/createproduct").post(createProduct);
+productRouter.route("/createproduct").post(protect, createProduct);
 
 export default productRouter;
