1254c9174b2229f5f279336844f271590a4d1c4e
Ankit Mishra <90400569+Ankit-Mishra07@users.noreply.github.com>
2022-09-09 01:18:11 +0530

    Added cart functionality with protected route

--- Task2-e-commerceCartFlow/backend/src/index.js
+++ Task2-e-commerceCartFlow/backend/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import productRouter from "./routes/productRoutes.js";
 import authRouter from "./routes/authRoutes.js";
+import cartRouter from "./routes/cartRoutes.js";
 
 const app = express();
 app.use(express.json());
@@ -9,4 +10,5 @@ app.use(cors());
 
 app.use("/api/product", productRouter);
 app.use("/api/auth", authRouter);
+app.use("/api/cart", cartRouter);
 export default app;
